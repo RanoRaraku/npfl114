8608c0763d6eb380a1e7a730e824043d3ca4747a
Michal Borsky <michal.borsky@protonmail.com>
2023-09-22 16:47:49 +0000

    seq2seq 1

--- competitions/tagger/model.py
+++ competitions/tagger/model.py
@@ -1,13 +1,8 @@
-import torch
 import torch.nn as nn
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence, pack_sequence, pad_sequence
-import time
-from typing import Optional, Any
 
 
 class SimpleRNN(nn.Module):
-    # zvysit dropout
-    # pridat chars
     def __init__(self, args):
         super(SimpleRNN, self).__init__()
         self.epoch = 0
@@ -56,82 +51,78 @@ class SimpleRNN(nn.Module):
         z = self.linear(x + y)
         return z
 
-    def eval_accuracy(self, dloader, loss_fn: Optional[Any] = None):
-        """
-        Returns accuracy and optionally per_sample loss.
-        """
-        self.eval()
-        total_loss, total_samples, corr = 0, 0, 0
-        for batch in dloader:
-            words_num = batch["words_num"].to(self.device)
-            words = batch["words"].to(self.device)
-            chars = batch["chars"]
-            tags = batch["tags"].to(self.device)
-            
-            max_seq_len = torch.max(words_num)
-            mask = torch.arange(max_seq_len, device=self.device).expand(
-                len(words_num), max_seq_len
-            ) < words_num.unsqueeze(1)
-            y_hat = self(words, words_num, chars)
-            corr += torch.sum(
-                torch.argmax(y_hat[mask], dim=-1) == torch.argmax(tags[mask], dim=-1)
+class Seq2Seq(nn.Module):
+
+    class Encoder(nn.Module):
+        def __init__(self, args):
+            super(Seq2Seq.Encoder, self).__init__()
+            self.epoch = 0
+            self.args = args
+            self.device = args["device"]
+
+            self.word_embedd = nn.Embedding(args["word_vocab_size"], args["we_dim"])
+            self.word_lstm = nn.LSTM(
+                input_size=args["we_dim"],
+                hidden_size=args["hidden_size"],
+                num_layers=args["num_layers"],
+                batch_first=True,
+                dropout=args["dropout"],
+                bidirectional=True,
             )
-            total_samples += torch.sum(words_num)
-
-            if loss_fn:
-                loss = loss_fn(y_hat[mask], tags[mask])
-                total_loss += loss.item()
-
-        return corr / total_samples, total_loss / len(dloader)
-
-    def train_epoch(
-        self, train_dloader, dev_dloader, loss_fn, optim, logger: Optional[Any] = None
-    ):
-        start_time = time.time()
-        self.train()
-        for batch in train_dloader:
-            words_num = batch["words_num"].to(self.device)
-            words = batch["words"].to(self.device)
-            chars = batch["chars"]
-            tags = batch["tags"].to(self.device)
-
-            max_words_num = torch.max(words_num)
-            mask = torch.arange(max_words_num, device=self.device).expand(
-                len(words_num), max_words_num
-            ) < words_num.unsqueeze(1)
-
-            # Run inference
-            y_hat = self(words, words_num, chars)
-            loss = loss_fn(y_hat[mask], tags[mask])
-
-            # Update params
-            optim.zero_grad()
-            loss.backward()
-            optim.step()
-
-            if logger:
-                logger.log({"train_loss": loss.item()})
-        self.epoch += 1
-
-        # log metrics to wandb
-        dev_acc, dev_loss = self.eval_accuracy(dev_dloader, loss_fn)
-        end_time = time.time()
-        if logger:
-            logger.log(
-                {
-                    "epoch_time": end_time - start_time,
-                    "dev_loss": dev_loss,
-                    "dev_acc": dev_acc,
-                }
+            self.char_embedd = nn.Embedding(args["char_vocab_size"], args["we_dim"])
+            self.char_lstm = nn.LSTM(
+                input_size=args["we_dim"],
+                hidden_size=2*args["hidden_size"],
+                num_layers=1,
+                batch_first=True,
+                dropout=args["dropout"],
+                bidirectional=False,
             )
 
-        # save checkpoint
-        torch.save(
-            {
-                "epoch": self.epoch,
-                "model_state_dict": self.state_dict(),
-                "optimizer_state_dict": optim.state_dict(),
-                "loss": loss.item(),
-            },
-            f"{self.epoch}.pt",
-        )
+        def forward(self, words, words_num, chars):
+            # word LSTM
+            x = self.word_embedd(words)
+            x = pack_padded_sequence(
+                x, words_num.to("cpu"), batch_first=True, enforce_sorted=False
+            )
+            x, _ = self.word_lstm(x)
+            x, _ = pad_packed_sequence(x, batch_first=True)
+
+            # Character LSTM
+            # embedd a word at a time where word is a sequence of characters
+            y = [[self.char_embedd(word.to(self.device)) for word in sent] for sent in chars]
+            # pack words in sentence into one packed sequence
+            y = [pack_sequence(embeddings, enforce_sorted=False) for embeddings in y]
+            # output hidden state h_n for last character in a word
+            y = [self.char_lstm(packed)[1][0].squeeze(0) for packed in y]
+            # pad to match dimensionality of word_lstm
+            y = pad_sequence(y, batch_first=True)
+
+            z = x + y
+            return z
+
+    class Decoder(nn.Module):
+        def __init__(self, args):
+            super(Seq2Seq.Decoder, self).__init__()
+            self.epoch = 0
+            self.args = args
+            self.device = args["device"]
+
+        def forward(self):
+            ...
+
+    def __init__(self, args):
+        super(Seq2Seq, self).__init__()
+        self.epoch = 0
+        self.args = args
+        self.device = args["device"]
+
+        self.encoder = Seq2Seq.Encoder(args)
+
+
+    def forward(self, words, words_num, chars):
+        
+        print(words.shape)
+        encoded = self.encoder(words, words_num, chars)
+        print(encoded.shape)
+        
--- competitions/tagger/train.py
+++ competitions/tagger/train.py
@@ -1,11 +1,94 @@
 import wandb
 import datetime
-
+import time
+from typing import Optional, Any
 import torch
 import torch.nn as nn
 
 from Morpho import MorphoDataset
-from model import SimpleRNN
+from model import SimpleRNN, Seq2Seq
+
+
+def eval_accuracy(model, dloader, loss_fn: Optional[Any] = None):
+    """
+    Returns accuracy and optionally per_sample loss.
+    """
+    model.eval()
+    total_loss, total_samples, corr = 0, 0, 0
+    for batch in dloader:
+        words_num = batch["words_num"].to(model.device)
+        words = batch["words"].to(model.device)
+        chars = batch["chars"]
+        tags = batch["tags"].to(model.device)
+        
+        max_seq_len = torch.max(words_num)
+        mask = torch.arange(max_seq_len, device=model.device).expand(
+            len(words_num), max_seq_len
+        ) < words_num.unsqueeze(1)
+        y_hat = model(words, words_num, chars)
+        corr += torch.sum(
+            torch.argmax(y_hat[mask], dim=-1) == torch.argmax(tags[mask], dim=-1)
+        )
+        total_samples += torch.sum(words_num)
+
+        if loss_fn:
+            loss = loss_fn(y_hat[mask], tags[mask])
+            total_loss += loss.item()
+
+    return corr / total_samples, total_loss / len(dloader)
+
+def train_epoch(model, optim, loss_fn, train_dataloader, dev_dataloader, logger):
+
+    start_time = time.time()
+    model.train()
+    for batch in train_dataloader:
+        words_num = batch["words_num"].to(model.device)
+        words = batch["words"].to(model.device)
+        chars = batch["chars"]
+        tags = batch["tags"].to(model.device)
+
+        max_words_num = torch.max(words_num)
+        mask = torch.arange(max_words_num, device=model.device).expand(
+            len(words_num), max_words_num
+        ) < words_num.unsqueeze(1)
+
+        # Run inference
+        y_hat = model(words, words_num, chars)
+        loss = loss_fn(y_hat[mask], tags[mask])
+
+        # Update params
+        optim.zero_grad()
+        loss.backward()
+        optim.step()
+
+        if logger:
+            logger.log({"train_loss": loss.item()})
+
+        exit()
+    model.epoch += 1
+
+    # log metrics to wandb
+    dev_acc, dev_loss = eval_accuracy(dev_dataloader, loss_fn)
+    end_time = time.time()
+    if logger:
+        logger.log(
+            {
+                "epoch_time": end_time - start_time,
+                "dev_loss": dev_loss,
+                "dev_acc": dev_acc,
+            }
+        )
+
+    # save checkpoint
+    torch.save(
+        {
+            "epoch": model.epoch,
+            "model_state_dict": model.state_dict(),
+            "optimizer_state_dict": optim.state_dict(),
+            "loss": loss.item(),
+        },
+        f"tagger.{model.epoch}.pt",
+    )
 
 
 morpho = MorphoDataset("czech_pdt")
@@ -27,18 +110,46 @@ simple_rnn_args = {
     "packed_sequences": True,
     "characters": True,
 }
+seq2seq_args = {
+    "batch_size":2,
+    "epochs":1,
+    "device":"cuda" if torch.cuda.is_available() else "cpu",
+    "dataset":"czech_pdt",
+    "model":"Seq2Seq",
+    "we_dim":128,
+    "hidden_size":128,
+    "num_layers":1,
+    "dropout":0.1,
+    "word_vocab_size": morpho.train.unique_forms,
+    "char_vocab_size": morpho.train.unique_chars,
+    "num_classes":morpho.train.unique_tags,
+    "label_smoothing":0.1,
+    "packed_sequences": True,
+    "characters": True,
+}
 
-print()
 
-model = SimpleRNN(simple_rnn_args).to(simple_rnn_args["device"])
+
+# 1) SimpleRNN
+#model = SimpleRNN(simple_rnn_args).to(simple_rnn_args["device"])
+# 2) Seq2Seq
+model = Seq2Seq(seq2seq_args).to(seq2seq_args["device"])
+
+
 optim = torch.optim.AdamW(model.parameters())
-loss_fn = nn.CrossEntropyLoss(label_smoothing=simple_rnn_args["label_smoothing"])
-train_dloader = morpho.train.to_dataloader(simple_rnn_args["batch_size"], shuffle=True)
-dev_dloader = morpho.dev.to_dataloader(simple_rnn_args["batch_size"], shuffle=False)
+loss_fn = nn.CrossEntropyLoss(label_smoothing=seq2seq_args["label_smoothing"])
+train_dloader = morpho.train.to_dataloader(seq2seq_args["batch_size"], shuffle=True)
+dev_dloader = morpho.dev.to_dataloader(seq2seq_args["batch_size"], shuffle=False)
 
-wandb.login()
-run_name =  datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
-wandb.init(project="tagger_competition", name=run_name, config=simple_rnn_args)
-for _ in range(simple_rnn_args["epochs"]):
-    model.train_epoch(train_dloader, dev_dloader, loss_fn, optim, wandb)
-wandb.finish()
+
+# wandb.login()
+# run_name =  datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+# wandb.init(project="tagger_competition", name=run_name, config=simple_rnn_args)
+# for _ in range(simple_rnn_args["epochs"]):
+#     train_epoch(model, train_dloader, dev_dloader, loss_fn, optim, wandb)
+# wandb.finish()
+
+
+for _ in range(seq2seq_args["epochs"]):
+    train_epoch(model, train_dloader, dev_dloader, loss_fn, optim, wandb)
+
