29a439aaf865ef342e2cd4f72101a4517bf8b559
Michal Borsky <Michal.Borsky@protonmail.com>
2023-09-28 14:01:42 +0000

    eval fix 2

--- competitions/tagger/model.py
+++ competitions/tagger/model.py
@@ -1,12 +1,14 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from torch.nn.utils.rnn import (
     pack_padded_sequence,
     pack_sequence,
     pad_packed_sequence,
     pad_sequence,
 )
+import pathlib
+import time
+from typing import Any, Optional
 
 
 class SimpleRNN(nn.Module):
@@ -194,3 +196,105 @@ class Seq2Seq(nn.Module):
         decoded = self.decoder(encoded, words_num, targets)
 
         return decoded
+
+
+def eval_accuracy(model, dloader, loss_fn: Optional[Any] = None):
+    """
+    Returns accuracy and optionally per_sample loss.
+    """
+    model.eval()
+    total_loss, total_samples, corr = 0, 0, 0
+    for batch in dloader:
+        words_num = batch["words_num"].to(model.device)
+        words = batch["words"].to(model.device)
+        chars = batch["chars"]
+        tags = batch["tags"].to(model.device)
+
+        max_seq_len = torch.max(words_num)
+        mask = torch.arange(max_seq_len, device=model.device).expand(
+            len(words_num), max_seq_len
+        ) < words_num.unsqueeze(1)
+        y_hat = model(words, words_num, chars)
+        corr += torch.sum(torch.argmax(y_hat[mask], dim=-1) == tags[mask])
+        total_samples += torch.sum(words_num)
+
+        if loss_fn:
+            loss = loss_fn(y_hat[mask], tags[mask])
+            total_loss += loss.item()
+
+    return corr / total_samples, total_loss / len(dloader)
+
+
+def load_checkpoint(path, model, optim):
+    last_checkpoint = 0
+    for f in pathlib.Path(".").iterdir():
+        if ".pt" in str(f) and int(f.stem) > last_checkpoint:
+            path = str(f)
+
+    checkpoint = torch.load(path)
+    model.load_state_dict(checkpoint["model_state_dict"])
+    optim.load_state_dict(checkpoint["optimizer_state_dict"])
+    epoch = checkpoint["epoch"]
+    loss = checkpoint["loss"]
+
+    model.epoch = epoch
+    return model, optim, loss
+
+
+def train_epoch(
+    model,
+    train_dataloader,
+    dev_dataloader,
+    loss_fn,
+    optim,
+    logger: Optional[Any] = None,
+):
+    start_time = time.time()
+    model.train()
+    for batch in train_dataloader:
+        words_num = batch["words_num"].to(model.device)
+        words = batch["words"].to(model.device)
+        chars = batch["chars"]
+        tags = batch["tags"].to(model.device)
+
+        max_words_num = torch.max(words_num)
+        mask = torch.arange(max_words_num, device=model.device).expand(
+            len(words_num), max_words_num
+        ) < words_num.unsqueeze(1)
+
+        # Run inference
+        y_hat = model(words, words_num, chars, tags)
+        loss = loss_fn(y_hat[mask], tags[mask])
+
+        # Update params
+        optim.zero_grad()
+        loss.backward()
+        optim.step()
+
+        if logger is not None:
+            logger.log({"train_loss": loss.item()})
+
+    model.epoch += 1
+
+    # log metrics to wandb
+    dev_acc, dev_loss = eval_accuracy(model, dev_dataloader, loss_fn)
+    end_time = time.time()
+    if logger is not None:
+        logger.log(
+            {
+                "epoch_time": end_time - start_time,
+                "dev_loss": dev_loss,
+                "dev_acc": dev_acc,
+            }
+        )
+
+    # save checkpoint
+    torch.save(
+        {
+            "epoch": model.epoch,
+            "model_state_dict": model.state_dict(),
+            "optimizer_state_dict": optim.state_dict(),
+            "loss": loss.item(),
+        },
+        f"tagger.{model.epoch}.pt",
+    )
--- competitions/tagger/train.py
+++ competitions/tagger/train.py
@@ -1,107 +1,59 @@
-import pathlib
-import time
-from typing import Any, Optional
+import torch.nn as nn
+import wandb
 
 import torch
 
-
-def eval_accuracy(model, dloader, loss_fn: Optional[Any] = None):
-    """
-    Returns accuracy and optionally per_sample loss.
-    """
-    model.eval()
-    total_loss, total_samples, corr = 0, 0, 0
-    for batch in dloader:
-        words_num = batch["words_num"].to(model.device)
-        words = batch["words"].to(model.device)
-        chars = batch["chars"]
-        tags = batch["tags"].to(model.device)
-
-        max_seq_len = torch.max(words_num)
-        mask = torch.arange(max_seq_len, device=model.device).expand(
-            len(words_num), max_seq_len
-        ) < words_num.unsqueeze(1)
-        y_hat = model(words, words_num, chars)
-        corr += torch.sum(torch.argmax(y_hat[mask], dim=-1) == tags[mask])
-        total_samples += torch.sum(words_num)
-
-        if loss_fn:
-            loss = loss_fn(y_hat[mask], tags[mask])
-            total_loss += loss.item()
-
-    return corr / total_samples, total_loss / len(dloader)
+from model import Seq2Seq, SimpleRNN, train_epoch
+from Morpho import MorphoDataset
+import datetime
 
 
-def load_checkpoint(path, model, optim):
-    last_checkpoint = 0
-    for f in pathlib.Path(".").iterdir():
-        if ".pt" in str(f) and int(f.stem) > last_checkpoint:
-            path = str(f)
+morpho = MorphoDataset("czech_pdt", add_sos_eos=True)
 
-    checkpoint = torch.load(path)
-    model.load_state_dict(checkpoint["model_state_dict"])
-    optim.load_state_dict(checkpoint["optimizer_state_dict"])
-    epoch = checkpoint["epoch"]
-    loss = checkpoint["loss"]
+simple_rnn_args = {
+    "batch_size": 2,
+    "epochs": 10,
+    "device": "cuda" if torch.cuda.is_available() else "cpu",
+    "dataset": "czech_pdt",
+    "model": "SimpleRNN",
+    "we_dim": 64,
+    "hidden_size": 128,
+    "num_layers": 1,
+    "dropout": 0.1,
+    "word_vocab_size": morpho.train.unique_forms,
+    "char_vocab_size": morpho.train.unique_chars,
+    "num_classes": morpho.train.unique_tags,
+    "label_smoothing": 0.1,
+    "packed_sequences": True,
+    "characters": True,
+}
+# seq2seq_args = {
+#     "batch_size": 64,
+#     "epochs": 10,
+#     "device": "cuda" if torch.cuda.is_available() else "cpu",
+#     "dataset": "czech_pdt",
+#     "model": "Seq2Seq",
+#     "we_dim": 256,
+#     "hidden_size": 1024,
+#     "num_layers": 3,
+#     "dropout": 0.1,
+#     "word_vocab_size": morpho.train.unique_forms,
+#     "char_vocab_size": morpho.train.unique_chars,
+#     "num_classes": morpho.train.unique_tags,
+#     "label_smoothing": 0.1,
+#     "packed_sequences": True,
+#     "characters": True,
+# }
 
-    model.epoch = epoch
-    return model, optim, loss
+# args = simple_rnn_args
+# model = SimpleRNN(args).to(args["device"])
+# #model = Seq2Seq(args).to(args["device"])
 
 
-def train_epoch(
-    model,
-    train_dataloader,
-    dev_dataloader,
-    loss_fn,
-    optim,
-    logger: Optional[Any] = None,
-):
-    start_time = time.time()
-    model.train()
-    for batch in train_dataloader:
-        words_num = batch["words_num"].to(model.device)
-        words = batch["words"].to(model.device)
-        chars = batch["chars"]
-        tags = batch["tags"].to(model.device)
+# optim = torch.optim.AdamW(model.parameters())
+# loss_fn = nn.CrossEntropyLoss(label_smoothing=args["label_smoothing"])
+# train_dloader = morpho.train.to_dataloader(args["batch_size"], shuffle=True)
+# dev_dloader = morpho.dev.to_dataloader(args["batch_size"], shuffle=False)
 
-        max_words_num = torch.max(words_num)
-        mask = torch.arange(max_words_num, device=model.device).expand(
-            len(words_num), max_words_num
-        ) < words_num.unsqueeze(1)
-
-        # Run inference
-        y_hat = model(words, words_num, chars, tags)
-        loss = loss_fn(y_hat[mask], tags[mask])
-
-        # Update params
-        optim.zero_grad()
-        loss.backward()
-        optim.step()
-
-        if logger is not None:
-            logger.log({"train_loss": loss.item()})
-
-    model.epoch += 1
-
-    # log metrics to wandb
-    dev_acc, dev_loss = eval_accuracy(model, dev_dataloader, loss_fn)
-    end_time = time.time()
-    if logger is not None:
-        logger.log(
-            {
-                "epoch_time": end_time - start_time,
-                "dev_loss": dev_loss,
-                "dev_acc": dev_acc,
-            }
-        )
-
-    # save checkpoint
-    torch.save(
-        {
-            "epoch": model.epoch,
-            "model_state_dict": model.state_dict(),
-            "optimizer_state_dict": optim.state_dict(),
-            "loss": loss.item(),
-        },
-        f"tagger.{model.epoch}.pt",
-    )
+# for _ in range(seq2seq_args["epochs"]):
+#     train_epoch(model, train_dloader, dev_dloader, loss_fn, optim)
